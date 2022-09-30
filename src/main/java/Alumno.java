43d6a218d7abb059fd53c546afe03b396d13ddd5
Brokolito <m.castro14@ufromail.cl>
2022-09-30 17:44:08 -0400

    Se intento xd

---
+++ src/main/java/Alumno.java
@@ -0,0 +1,13 @@
+public class Alumno {
+        private int rut;
+        private String nombre;
+        private String apellido;
+        private String carrera;
+
+        public Alumno(int rut, String nombre, String apellido, String carrera){
+        this.rut=rut;
+        this.nombre=nombre;
+        this.apellido=apellido;
+    }
+
+}
---
+++ src/main/java/Curso.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+public class Curso {
+    public Curso(String nombre) {
+        this.nombre = nombre;
+    }
+
+    private String nombre;
+    private List<Alumno> alumnos;
+    private int modulo;
+    private String carrera;
+    public Curso(){
+
+    }
+    }
+
