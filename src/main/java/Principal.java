ca7626fb020120186bea7a5aaeac08c375df6748
Brokolito <m.castro14@ufromail.cl>
2022-10-01 20:15:22 -0400

    Se termino gloriosamente

---
+++ src/main/java/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String[] args) {
+        Inicializar();
+    }
+public static void Inicializar(){
+Curso curso=new Curso("Marcelo",1,"ICI-I");
+curso.AddAlumno(new Alumno(21073339-6, curso.getNombre(), "Castro", curso.getCarrera()));
+
+}
+}
